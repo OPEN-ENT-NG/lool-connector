2fcc169d812a900840bc3d227872bc167f20c13a
florent.mariotti <florent.mariotti@cgi.com>
2023-07-28 14:23:30 +0200

    feat(home) : #LOOL-90 fix homepage file tree which could be folded.

--- src/main/resources/public/ts/controllers/home.ts
+++ src/main/resources/public/ts/controllers/home.ts
@@ -11,7 +11,7 @@ interface ViewModel {
     },
     workspaceTrees: Tree[],
     listFolders: any,
-    openedFolder: Element,
+    openedFolder: Array<Element>,
     selectedFolder: Element,
     documentTypeList: any[],
     eventer: Eventer,
@@ -42,7 +42,7 @@ export const homeController = ng.controller('HomeController', ['$scope',
         };
         vm.workspaceTrees = [];
         vm.listFolders = {};
-        vm.openedFolder = null;
+        vm.openedFolder = [];
         vm.selectedFolder = null;
         vm.documentTypeList = [];
         vm.display = {
@@ -70,24 +70,23 @@ export const homeController = ng.controller('HomeController', ['$scope',
                 isDisabled(folder) {
                     return false;
                 },
-                isOpenedFolder(folder) {
-                    if (vm.openedFolder === folder) {
-                        return true;
-                    }
-                    else if ((folder as Tree).filter) {
-                        if (!workspace.v2.service.isLazyMode()){
-                            return true;
-                        }
-                    }
-                    return vm.openedFolder && workspace.v2.service.findFolderInTreeByRefOrId(folder, vm.openedFolder);
+                isOpenedFolder(folder: Element): boolean {
+                    return vm.openedFolder.some((openFolder: Element) => openFolder === folder);
                 },
-                isSelectedFolder(folder) {
+                isSelectedFolder(folder: Element) {
                     return vm.selectedFolder === folder;
                 },
-                openFolder(folder) {
+                openFolder(folder: Element) {
                     vm.newDocument.folder = folder._id;
-                    vm.openedFolder = vm.selectedFolder = folder;
-                }
+                    vm.selectedFolder = folder;
+                    if (!this.isOpenedFolder(folder)) {
+                        vm.openedFolder = vm.openedFolder.filter((e: Element) => e != folder);
+                        vm.openedFolder.push(folder);
+                    }
+                    else {
+                        vm.openedFolder = vm.openedFolder.filter((e: Element) => e != folder);
+                    }
+                },
             }
 
             const {templates} = Behaviours.applicationsBehaviours[BEHAVIOURS_NAME].provider;
