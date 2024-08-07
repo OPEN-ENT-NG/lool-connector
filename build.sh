321f6c6a25f5d099b5425b8c8042c3f5bba3e4a8
Guillaume <72859932+Glama107@users.noreply.github.com>
2024-08-07 14:17:21 +0200

    feat(migration): #LOOL-102 Maven Migration

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+MVN_OPTS="-Duser.home=/var/maven"
 
 if [ ! -e node_modules ]
 then
@@ -18,8 +19,13 @@ case `uname -s` in
     fi
 esac
 
+init(){
+  me=`id -u`:`id -g`
+  echo"DEFAULT_DOCKER_USER=$me"> .env
+}
+
 clean () {
-  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle clean
+  docker compose run --rm maven mvn $MVN_OPTS clean
 }
 
 buildNode () {
@@ -32,8 +38,8 @@ buildNode () {
   esac
 }
 
-buildGradle () {
-  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle shadowJar install publishToMavenLocal
+install () {
+  docker compose run --rm maven mvn $MVN_OPTS install -DskipTests
 }
 
 testNode () {
@@ -60,19 +66,22 @@ testNodeDev () {
   esac
 }
 
-testGradle() {
-  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle test --no-build-cache --rerun-tasks
+test() {
+  docker compose run --rm maven mvn $MVN_OPTS test
 }
 
-publish () {
-  if [ -e "?/.gradle" ] && [ ! -e "?/.gradle/gradle.properties" ]
-  then
-    echo "odeUsername=$NEXUS_ODE_USERNAME" > "?/.gradle/gradle.properties"
-    echo "odePassword=$NEXUS_ODE_PASSWORD" >> "?/.gradle/gradle.properties"
-    echo "sonatypeUsername=$NEXUS_SONATYPE_USERNAME" >> "?/.gradle/gradle.properties"
-    echo "sonatypePassword=$NEXUS_SONATYPE_PASSWORD" >> "?/.gradle/gradle.properties"
-  fi
-  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle publish
+publish() {
+    version=`docker-compose run --rm maven mvn $MVN_OPTS help:evaluate -Dexpression=project.version -q -DforceStdout`
+    level=`echo $version | cut -d'-' -f3`
+    case "$level" in
+        *SNAPSHOT)
+            export nexusRepository='snapshots'
+            ;;
+        *)
+            export nexusRepository='releases'
+            ;;
+    esac
+    docker-compose run --rm maven mvn -DrepositoryId=ode-$nexusRepository -DskiptTests -Dmaven.test.skip=true --settings /var/maven/.m2/settings.xml deploy
 }
 
 for param in "$@"
@@ -84,17 +93,17 @@ do
     buildNode)
       buildNode
       ;;
-    buildGradle)
-      buildGradle
+    buildMaven)
+      install
       ;;
     install)
-      buildNode && buildGradle
+      buildNode && install
       ;;
     publish)
       publish
       ;;
     test)
-      testNode ; testGradle
+      testNode ; test
       ;;
     testNode)
       testNode
@@ -102,8 +111,8 @@ do
     testNodeDev)
       testNodeDev
       ;;
-    testGradle)
-      testGradle
+    test)
+      test
       ;;
     *)
       echo "Invalid argument : $param"
