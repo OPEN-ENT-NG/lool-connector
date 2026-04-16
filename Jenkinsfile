92149ac9f1df1db2bf10eb4677e36456c951cf6e
Junior Bernard <junior.bernard@opendigitaleducation.com>
2026-04-16 13:51:03 +0200

    chore: build image

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,11 +3,17 @@
 pipeline {
     agent any
 
+    environment {
+        NPM_TOKEN = credentials('npm-token')
+        TIPTAP_PRO_TOKEN = credentials('tiptap-pro-token')
+    }
+
     stages {
         stage('Initialization') {
             steps {
                 script {
-                    def version = sh(returnStdout: true, script: 'docker compose -f backend/docker-compose.yml run --rm maven mvn $MVN_OPTS help:evaluate -Dexpression=project.version -q -DforceStdout').trim()
+                    sh './build.sh init'
+                    def version = sh(returnStdout: true, script: 'docker run --rm -u `id -u`:`id -g` --env MAVEN_CONFIG=/var/maven/.m2 -w /usr/src/maven -v ./backend/:/usr/src/maven -v ~/.m2:/var/maven/.m2  opendigitaleducation/mvn-java8-node20:latest mvn -Duser.home=/var/maven help:evaluate -Dexpression=project.version -DforceStdout -q')
                     buildName "${env.GIT_BRANCH.replace('origin/', '')}@${version}"
                 }
             }
@@ -42,6 +48,11 @@ pipeline {
                     sh './build.sh init clean build publish'
                     sh 'rm -rf ../frontend/dist-home'
                 }
+            }
+        }
+        stage('Build image') {
+            steps {
+            sh './edifice image --rebuild=false'
             }
         }
     }
--- build.sh
+++ build.sh
@@ -1,21 +1,58 @@
 #!/bin/bash
 
-# Frontend
-cd frontend
-./build.sh --no-docker clean init build
-cd ..
+# If DEBUG env var is set to "true" then set -x to enable debug mode
+if [ "$DEBUG" == "true" ]; then
+	set -x
+	EDIFICE_CLI_DEBUG_OPTION="--debug"
+else
+	EDIFICE_CLI_DEBUG_OPTION=""
+fi
 
-# Create directory structure and copy frontend dist
-cd backend
-find ./src/main/resources/public/ -maxdepth 1 -type f -exec rm -f {} +
-
-init(){
+init() {
   me=`id -u`:`id -g`
   echo "DEFAULT_DOCKER_USER=$me" > .env
+
+  # If CLI_VERSION is empty set to latest
+  if [ -z "$CLI_VERSION" ]; then
+    CLI_VERSION="latest"
+  fi
+  # Create a build.compose.yaml file from following template
+  cat <<EOF > build.compose.yaml
+services:
+  edifice-cli:
+    image: opendigitaleducation/edifice-cli:$CLI_VERSION
+    user: "$DEFAULT_DOCKER_USER"
+EOF
+  # Copy /root/edifice from edifice-cli container to host machine
+  docker compose -f build.compose.yaml create edifice-cli
+  docker compose -f build.compose.yaml cp edifice-cli:/root/edifice ./edifice
+  docker compose -f build.compose.yaml rm -fsv edifice-cli
+  rm -f build.compose.yaml
+  chmod +x edifice
+  ./edifice version $EDIFICE_CLI_DEBUG_OPTION
 }
 
-# Build backend
-./build.sh --no-docker clean build
+# If called without arguments, run the full local build
+if [ "$#" -eq 0 ]; then
+
+  # Frontend
+  cd frontend
+  ./build.sh --no-docker clean init build
+  cd ..
+
+  # Create directory structure and copy frontend dist
+  cd backend
+  find ./src/main/resources/public/ -maxdepth 1 -type f -exec rm -f {} +
+
+
+  # Build backend
+  ./build.sh --no-docker clean build
+fi
+
+
+if [ ! -e .env ]; then
+  init
+fi
 
 buildNode () {
   case `uname -s` in
